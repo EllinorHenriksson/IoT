8d031dbb8b584f88f16fdc1b641952947ed2a282
Ellinor Henriksson <eh224kr@student.lnu.se>
2023-06-13 16:52:53 +0200

    Add example file for credentials

---
+++ example_project_secrets.py
@@ -0,0 +1,6 @@
+# This is an example file. Create a file called project_secrets.py and replace the values of the variables down below with the correct ones.
+
+ADAFRUIT_USERNAME = "username" # Username for the adafruit account
+ADAFRUIT_PASSWORD = "password" # Password for the adafruit account
+SSID = "name" # Name of the wifi
+SSID_PASSWORD = "password" # Password for the wifi
